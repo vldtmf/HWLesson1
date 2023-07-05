91170575004ac3df8327fe40919037432c4fe679
vldtmf <vdovaclico@gmail.com>
2023-07-06 00:40:36 +0300

    Добавил решение задачи 6

--- HWTask4/Program.cs
+++ HWTask4/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// написать программу по которой на вход принимается три числа, а на 
+// выходе выдаётся максимальное из них
+ Console.WriteLine("Введите первое число");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int b = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите третье число");
+int c = int.Parse(Console.ReadLine());
+int max = a;
+if(a > max) max = a;
+if(b > max) max = b;
+if(c > max) max = c;
+    Console.WriteLine($"Максимальное число {max}");
