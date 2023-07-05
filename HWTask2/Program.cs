1b646d7a08c7fe7225ea9a0c8103138a039a0e37
vldtmf <vdovaclico@gmail.com>
2023-07-06 00:21:44 +0300

    Задача №2 выполнена

--- HWTask2/Program.cs
+++ HWTask2/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Составить программу, которая на вход принимает два числа
+//а на выходе выдает какое число больше
+Console.WriteLine("Введите первое число");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int b = int.Parse(Console.ReadLine());
+
+if(a > b){
+    Console.WriteLine($"Больше число {a}");
+}
+else{
+    Console.WriteLine($"Больше число {b}");
+}
