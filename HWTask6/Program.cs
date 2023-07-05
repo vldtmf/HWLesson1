780cca325a4135072cbf8b476bf4d909cdc93765
vldtmf <vdovaclico@gmail.com>
2023-07-06 01:17:21 +0300

    Добавил решение задачи 6

--- HWTask6/Program.cs
+++ HWTask6/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// На вход принимается число, в ответ является ли число чётным (да-нет)
+Console.WriteLine("Введите число");
+int a = int.Parse(Console.ReadLine());
+double b = a / 2;
+double c = b * 2;
+if(c == a){
+    Console.WriteLine("Введённое число чётное");
+}
+else{
+    Console.WriteLine("Введённое число нечётное");
+}
+
